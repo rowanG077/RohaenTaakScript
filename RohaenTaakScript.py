facfd2fd18311043add7b12f0777c7d9cfa044b6
Rowan Goemans <RB.Goemans@student.han.nl>
2017-11-12 17:11:02 +0100

    -- Changed json for hierarchy. -- Added structure to create epic -> tasks -> sub-tasks. -- Sum complexity from all sub-tasks complexities. -- Added epic link. -- Remove issue links -- Create issue under sprint -- Possibility to create task/sub-tasks under existing epics/tasks

--- RohaenTaakScript.py
+++ RohaenTaakScript.py
@@ -1,49 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
 import requests
 import json
 import copy
 
 baseLine = {
-	"Complexity": 261.00,
-	"Minutes": 7680.0,
+	"Complexity": 0.0,
+	"Minutes": 21600.0,
 }
 
-auth = ("name", "pw")
+auth = ("username", "password")
+
+boardId = "522"
 
 base = {
 	"fields": {
-	   "project":
-	   { 
-		  "key": "projectName"
-	   },
-	   "summary": "test desciption",
-	   "description": "",
-	   "issuetype": {
-		  "name": "Task"
-	   },
-	   "timetracking":
+		"project":
 		{
-		   "originalEstimate": "",
-		   "remainingEstimate": ""
+			"key": "FRGDXL"
+		},
+		"customfield_10301": "1261", #sprint to create the issue under
+		"summary": "test description",
+		"description": "",
+		"issuetype": {
+			"name": "Task"
+		},
+		"priority": {
+			"name": "Major"
+		},
+		"timetracking":
+		{
+			"originalEstimate": "",
+			"remainingEstimate": ""
 		}
-   }
+	}
 }
 
+def formatDescription(issue):
+	return "Detailed: {}\n\nPreconditions: {}\n\nPostConditions: {}\n\nNeeded resources: {}".format(issue["Detailed"], issue["Preconditions"], issue["PostConditions"], issue["NeededResources"])
 
-def createIssue(summary, description, complexity, issueType, parent = None):
-	timeEstimate = int((baseLine["Minutes"] / baseLine["Complexity"]) * complexity)
+def createIssue(issue, issueType, parent = None):
+	if issue["Id"] != None:
+		return issue["Id"];
+
+	timeEstimate = int((baseLine["Minutes"] / baseLine["Complexity"]) * issue["Complexity"])
 	data = copy.deepcopy(base)
-	if parent != None and issueType == "Sub-task":
-		data["fields"]["parent"] = {
-			"id": parent
-		}
+
+	if issueType == "Epic":
+		data["fields"]["customfield_10401"] = issue["EpicName"]
+
+	if parent != None:
+		if issueType == "Sub-task":
+			data["fields"]["parent"] = { "key": parent }
+		elif issueType == "Task":
+			data["fields"]["customfield_10400"] = parent
 
 	data["fields"]["issuetype"]["name"] = issueType
-	data["fields"]["summary"] = summary
-	data["fields"]["description"] = description
+	data["fields"]["summary"] = issue["Summary"]
+	data["fields"]["description"] = formatDescription(issue)
 	data["fields"]["timetracking"]["originalEstimate"] = str(timeEstimate) + "m"
 	data["fields"]["timetracking"]["remainingEstimate"] = str(timeEstimate) + "m"
-
-	print json.dumps(data)
 
 	result = requests.post("http://jira.icaprojecten.nl/rest/api/2/issue/", auth = auth, json = data)
 	if result.status_code < 200 or result.status_code >= 300:
@@ -51,310 +68,87 @@ def createIssue(summary, description, complexity, issueType, parent = None):
 		print result.text
 		raise Exception('A very specific bad thing happened')
 
-	createdId = result.json()["id"]
+	print issueType + ", id: " + result.json()["key"] + ", " + issue["Summary"]
 
-	if parent != None and issueType == "Task":
-		data = {
-			"type": {
-				"name": "Relates"
-			},
-			"inwardIssue": {
-				"id": createdId
-			},
-			"outwardIssue": {
-				"id": parent
-			},
-			"comment": {
-				"body": "User story link",
-			}
-		}
-		result = requests.post("http://jira.icaprojecten.nl//rest/api/2/issueLink", auth=("rb.goemans@student.han.nl", "1991-11-28"), json = data)
-		if result.status_code < 200 or result.status_code >= 300:
-			print result.status_code
-			print result.text
-			raise Exception('A very specific bad thing happened')
+	return result.json()["key"]
 
-	return createdId
 
-stories = [
+def writeToCsv(stuff):
+	csv = open("workfile","w") 
+	for epic in stuff:
+		csv.write("epic: " + epic["Summary"] + "\n")
+		for story in epic["Children"]:
+			csv.write("Task: " + story["Summary"] + "\n")
+			for task in story["Children"]:
+				csv.write("Sub-task: " + task["Summary"] + "\n")
+	csv.close()
+
+def createIssues(stuff):
+	for epic in stuff:
+		epic["Id"] = createIssue(epic, "Epic")
+
+		for story in epic["Children"]:
+			story["Id"] = createIssue(story, "Task", epic["Id"])
+
+			for task in story["Children"]:
+				task["Id"] = createIssue(task, "Sub-task", story["Id"])
+
+def getSprints(boardId):
+	result = requests.get("http://jira.icaprojecten.nl/rest/agile/1.0/board/" + boardId + "/sprint", auth = auth)
+	if result.status_code < 200 or result.status_code >= 300:
+		print result.status_code
+		print result.text
+		raise Exception('A very specific bad thing happened')
+
+	return result.json()
+
+
+issues = [
 	{
 		"Id": None,
-		"Summary": "Uitvoeren expirement",
-		"Description": "",
+		"EpicName": "Some epic name",
+		"Summary": "Foo epic",
+		"Detailed": "Detailed description for this epic",
+		"Preconditions": "Ultimate programmer skills",
+		"PostConditions": "Ultimate UML skills",
+		"NeededResources": "Internet",
+		"Priority": "Major",
 		"Complexity": 0,
-		"Tasks": [
-			{
+		"Children": [{
+			"Id": None,
+			"Summary": "some task",
+			"Detailed": "",
+			"Preconditions": "",
+			"PostConditions": "",
+			"NeededResources": "",
+			"Priority": "Major",
+			"Complexity": 0,
+			"Children": [{
 				"Id": None,
-				"Summary": "SRS opstellen.",
-				"Description": "",
-				"Complexity": 0,
-				"SubTask": [
-					{
-						"Id": None,
-						"Summary": "Introductie schrijven.",
-						"Description": "",
-						"Complexity": 3
-					},
-					{
-						"Id": None,
-						"Summary": "Domain model opstellen.",
-						"Description": "",
-						"Complexity": 3
-					},
-					{
-						"Id": None,
-						"Summary": "Use case diagram maken.",
-						"Description": "",
-						"Complexity": 3
-					},
-					{
-						"Id": None,
-						"Summary": "fully dressed use case.",
-						"Description": "",
-						"Complexity": 5
-					},
-					{
-						"Id": None,
-						"Summary": "System sequence diagram opstellen.",
-						"Description": "",
-						"Complexity": 8
-					},
-					{
-						"Id": None,
-						"Summary": "Operation contracts opstellen.",
-						"Description": "",
-						"Complexity": 5
-					},
-					{
-						"Id": None,
-						"Summary": "Functionele en niet functionele requirements opstellen.",
-						"Description": "",
-						"Complexity": 5
-					}
-				]
-			},
-			{
+				"Summary": "Some small task to handle",
+				"Detailed": "",
+				"Preconditions": "",
+				"PostConditions": "",
+				"NeededResources": "",
+				"Priority": "Major",
+				"Complexity": 10
+			},{
 				"Id": None,
-				"Summary": "Onderzoek naar Discrete event simulation.",
-				"Description": "",
-				"Complexity": 0,
-				"SubTask": [
-					{
-						"Id": None,
-						"Summary": "Opstellen Hoofd- en deelvragen.",
-						"Description": "",
-						"Complexity": 5
-					},
-					{
-						"Id": None,
-						"Summary": "Deelvragen uitwerken.",
-						"Description": "",
-						"Complexity": 13
-					},
-					{
-						"Id": None,
-						"Summary": "Inleiding en conclusie schrijven.",
-						"Description": "",
-						"Complexity": 5
-					}
-				]
-			},
-			{
-				"Id": None,
-				"Summary": "SDD opstellen.",
-				"Description": "",
-				"Complexity": 0,
-				"SubTask": [
-					{
-						"Id": None,
-						"Summary": "Architectuur plaatje maken.",
-						"Description": "",
-						"Complexity": 5
-					},
-					{
-						"Id": None,
-						"Summary": "Deployment diagram maken.",
-						"Description": "",
-						"Complexity": 8
-					},
-					{
-						"Id": None,
-						"Summary": "TLCP protocol design.",
-						"Description": "",
-						"Complexity": 8
-					},
-					{
-						"Id": None,
-						"Summary": "Logging formaat maken.",
-						"Description": "",
-						"Complexity": 5
-					},
-					{
-						"Id": None,
-						"Summary": "Configuratie parameters design maken.",
-						"Description": "",
-						"Complexity": 8
-					},
-					{
-						"Id": None,
-						"Summary": "Simulator UML.",
-						"Description": "",
-						"Complexity": 13
-					},
-					{
-						"Id": None,
-						"Summary": "Simulator design decisions in kaart brengen en verantwoorden.",
-						"Description": "",
-						"Complexity": 8
-					},
-					{
-						"Id": None,
-						"Summary": "Scheduler UML.",
-						"Description": "",
-						"Complexity": 8
-					},
-					{
-						"Id": None,
-						"Summary": "Scheduler design decisions in kaart brengen en verantwoorden.",
-						"Description": "",
-						"Complexity": 5
-					}
-				]
-			},
-			{
-				"Id": None,
-				"Summary": "Implementeren Scheduler component.",
-				"Description": "",
-				"Complexity": 0,
-				"SubTask": [
-					{
-						"Id": None,
-						"Summary": "Opstellen project met git, jenkins en sonarcube.",
-						"Description": "",
-						"Complexity": 5
-					},
-					{
-						"Id": None,
-						"Summary": "Round robin scheduling Implementeren.",
-						"Description": "",
-						"Complexity": 8
-					},
-					{
-						"Id": None,
-						"Summary": "TLCP protocol integreren.",
-						"Description": "",
-						"Complexity": 8
-					},
-					{
-						"Id": None,
-						"Summary": "Logging implementeren.",
-						"Description": "",
-						"Complexity": 5
-					}
-				]
-			},
-			{
-				"Id": None,
-				"Summary": "Implementeren Simulatie component.",
-				"Description": "",
-				"Complexity": 0,
-				"SubTask": [
-					{
-						"Id": None,
-						"Summary": "Opstellen project met git, jenkins en sonarcube",
-						"Description": "",
-						"Complexity": 5
-					},
-					{
-						"Id": None,
-						"Summary": "Genereren autos volgens normaalverdeling rond spitsen.",
-						"Description": "",
-						"Complexity": 13
-					},
-					{
-						"Id": None,
-						"Summary": "Implementeren stoplichten en rijbanen.",
-						"Description": "",
-						"Complexity": 8
-					},
-					{
-						"Id": None,
-						"Summary": "Implementeren wegrijden en aankomen autos.",
-						"Description": "",
-						"Complexity": 13
-					},
-					{
-						"Id": None,
-						"Summary": "Implementeren configuratie mogelijkheid.",
-						"Description": "",
-						"Complexity": 8
-					}
-				]
-			},
-			{
-				"Id": None,
-				"Summary": "Implementeren TLCP protocol en netwerk communicatie.",
-				"Description": "",
-				"Complexity": 0,
-				"SubTask": [
-					{
-						"Id": None,
-						"Summary": "Implementeren TLCP commands.",
-						"Description": "",
-						"Complexity": 8
-					},
-					{
-						"Id": None,
-						"Summary": "Parseren en serializen van TLCP commands uit/naar strings.",
-						"Description": "",
-						"Complexity": 8
-					},
-					{
-						"Id": None,
-						"Summary": "Implementeren server die TLCP commands ontvangt en verstuurd.",
-						"Description": "",
-						"Complexity": 8
-					}
-					,
-					{
-						"Id": None,
-						"Summary": "Implementeren client die TLCP commands ontvangt en verstuurd.",
-						"Description": "",
-						"Complexity": 8
-					}
-				]
-			},
-			{
-				"Id": None,
-				"Summary": "Acceptance testen.",
-				"Description": "",
-				"Complexity": 0,
-				"SubTask": [
-					{
-						"Id": None,
-						"Summary": "Testplan schrijven.",
-						"Description": "",
-						"Complexity": 20
-					},
-					{
-						"Id": None,
-						"Summary": "Testplan uitvoeren.",
-						"Description": "",
-						"Complexity": 13
-					}
-				]
-			}
-		]
+				"Summary": "some small task to handle 2",
+				"Detailed": "",
+				"Preconditions": "",
+				"PostConditions": "",
+				"NeededResources": "",
+				"Priority": "Major",
+				"Complexity": 40
+			}]
+		}]
 	}
 ]
 
-for story in stories:
-	if story["Id"] == None:
-		story["Id"] = createIssue(story["Summary"], story["Description"], story["Complexity"], "Story")
-	
-	for task in story["Tasks"]:
-		if task["Id"] == None:
-			task["Id"] = createIssue(task["Summary"], task["Description"], task["Complexity"], "Task", story["Id"])
-		
-		for subTasks in task["SubTask"]:
-			subTasks["Id"] = createIssue(subTasks["Summary"], subTasks["Description"], subTasks["Complexity"], "Sub-task", task["Id"])
+
+baseLine["Complexity"] = sum([subTask["Complexity"] for epic in issues for task in epic["Children"] for subTask in task["Children"]])
+
+print baseLine
+
+#createIssues(issues)
